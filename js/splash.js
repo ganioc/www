6f719451302c19ec4ad78e912338bf1da8f82265
spike yang <ganioc.yang@gmail.com>
2015-04-28 07:30:44 +0800

    add splash screen

--- js/splash.js
+++ js/splash.js
@@ -1,6 +1,6 @@
-$(document).on('pageinit','#splash',function(){
-// the .on() method does require jQuery 1.7 + but this will allow you to have the contained code only run when the #splash page is initialized.
+
   window.setTimeout(function(){
-    $.mobile.changePage("home.html", "fade");
+    $.mobile.changePage("home.html", "fade",false,false);
   }, 3000);
-});
+
+
