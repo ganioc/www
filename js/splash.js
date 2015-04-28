0483bb9f665681503b6487da86d8ad1ae428ff76
spike yang <ganioc.yang@gmail.com>
2015-04-28 23:01:27 +0800

    UI page ready. Next will realize the firework simulation.

--- js/splash.js
+++ js/splash.js
@@ -2,10 +2,11 @@
     var isPhoneGapReady = false;
     var durationSplash = 4000;
     var canvas, ctx;
-
+  var currentSimu ="";
+  var currentSimuName = "";
     
     function hideSplash(){
-        jq.mobile.changePage("#home", "fade");
+        jq.mobile.changePage("#home", "fade",false,false);
     }
     
     
@@ -20,8 +21,9 @@
 
     // configuration:
     function init(){
-        document.addEventListener('deviceready',onDeviceReady,false);
-        splash.fadeIn();
+      document.addEventListener('deviceready',onDeviceReady,false);
+      
+      splash.fadeIn();
     }
     function onDeviceReady(){
         isPhoneGapReady = true;
@@ -42,5 +44,36 @@
     window.splash = splash;
 
     window.onload = init;
+
+  jq(document).ready(function(){
+        canvas = document.getElementById('board_canvas');
+        ctx = canvas.getContext('2d');
+        ctx.canvas.width = window.innerWidth;
+        ctx.canvas.height = window.innerHeight - jq('#baord-header').height();
+        ctx.fillStyle = 'black';
+        ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
+
+    jq('#board').on('pageshow',function(event){
+      console.log('board in');
+      //event.preventDefault();
+      jq('#board-header h1').html(currentSimuName);
+
+    }).on('pagehide',function(event){
+      console.log('board out');
+      //event.preventDefault();
+    });
+
+    jq('#simu-list .simu-item').click(
+      function(event){
+        console.log(jq(this).children('.note').html());
+        currentSimu = jq(this).children('.note').html();
+        currentSimuName = jq(this).children('h4').html();
+        console.log(currentSimuName);
+        //event.preventDefault();
+      }
+    );
+
+  });
+
 })();
   
